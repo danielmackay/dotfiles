50c9900a642cf890294c90ac1732a3f98926652f
Daniel Mackay <daniel.mackay@live.com>
2022-09-20 22:37:27 +1000

    🐛 Fixed oh-my-posh setup

--- pwsh/src/plugins.ps1
+++ pwsh/src/plugins.ps1
@@ -1,9 +1,9 @@
-Invoke-Expression (oh-my-posh --init --shell pwsh --config "$PSScriptRoot/.omp.json")
+Invoke-Expression (oh-my-posh --init --shell pwsh --config "$PSScriptRoot/.hanselman.omp.json")
 
 Import-Module -Name Terminal-Icons
 
 # setup autocompletes
 Import-Module PSReadLine
 Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
+#Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
--- setup-shell.ps1
+++ setup-shell.ps1
@@ -1,7 +1,7 @@
-Write-Host "Git: add customisations"
+Write-Host "➡️ Git: add customisations"
 & git config --global include.path $pwd\git\.gitconfig
 
-Write-Host "Powershell: get modules"
+Write-Host "➡️ Powershell: get modules"
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 Install-Module `
     -Name @('Terminal-Icons') `
@@ -9,16 +9,16 @@ Install-Module `
 
 Install-Module PSReadLine -RequiredVersion 2.1.0
 
-Write-Host  "Windows Terminal: Updating PWSH profile"
+Write-Host  "➡️ Windows Terminal: Updating PWSH profile"
 $runCmd = ". `"$pwd\\pwsh\\run.ps1`"";
 If((Select-String -Path $PROFILE -Pattern $runCmd -SimpleMatch).Length -eq 0)
 {
-    Write-Host 'Adding bootstrap script to $PROFILE'
+    Write-Host '➡️ Adding bootstrap script to $PROFILE'
     Add-Content -Path $PROFILE -Value $([Environment]::NewLine) -NoNewline
     Add-Content -Path $PROFILE -Value $runCmd
 }
 
 # replace font in Windows Terminal
-Write-Host "Windows Terminal: Setting Nerd Font"
-Install-Module -Name MSTerminalSettings -AllowPrerelease
-Get-MSTerminalProfile | Set-MSTerminalProfile -FontFace "CaskaydiaCove Nerd Font"
+# Write-Host "Windows Terminal: Setting Nerd Font"
+# Install-Module -Name MSTerminalSettings
+# Get-MSTerminalProfile | Set-MSTerminalProfile -FontFace "CaskaydiaCove Nerd Font"
